f079e37d0463c4514f411e2c18fd5fa2a6952411
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-05-22 17:04:40 -0700

    add styling to products component

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Card, CardActions, CardContent, CardHeader, Typography, CardMedia, Container, Button } from "@mui/material";
 
 function Products() {
 
@@ -8,12 +9,34 @@ function Products() {
   // const dispatch = useDispatch();
 
   return(
-    categoryState.activeCategory.name ? 
-    productState.allProducts.map(product => {
-      if(product.category === categoryState.activeCategory.name){
-        return product.name        
-      }
-    })
+    categoryState.activeCategory.name ?
+    <Container id='productsContainer'>
+      {productState.allProducts.map(product => {
+        if(product.category === categoryState.activeCategory.name){
+          return <Card sx={{width: 300, height: 300, margin: '1rem'}}>
+            <CardHeader 
+              title={product.name}
+              subheader={product.price}
+            />
+            <CardMedia
+              sx={{height: 100}} 
+              image='https://placehold.co/200.png'
+            />
+            <CardContent>
+              <Typography variant="body2">
+                {product.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained">
+                Add To Cart
+              </Button>
+            </CardActions>
+          </Card>        
+        }
+      })}      
+    </Container> 
+
     : 
     productState.allProducts.map(product => product.name)
   )
