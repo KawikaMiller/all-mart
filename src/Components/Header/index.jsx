8a97e0093ac1291c411b8e098a26cbfc6f14ae38
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-05-23 11:24:31 -0700

    update header with cart

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header(props) {
 
   return(
-    <Container component='header' id='storeHeader'>
-      <Typography variant="h2" component='h2'>Pet Store</Typography>
-    </Container>
+    <Box component='header' id='storeHeader' sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Pet Store
+          </Typography>
+          <Button color="inherit">Cart</Button>
+        </Toolbar>
+      </AppBar>      
+    </Box>
   )
 
 }
