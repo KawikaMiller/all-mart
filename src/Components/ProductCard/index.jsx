7cb7797ac3d4fd8f671fc7916aebccf5b5a90a1b
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-07-31 06:37:37 -0700

    remove unused imports

--- src/Components/ProductCard/index.jsx
+++ src/Components/ProductCard/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { Card, CardActions, CardHeader, CardMedia, Button, Typography } from "@mui/material";
+import { Card, CardActions, CardHeader, CardMedia, Button } from "@mui/material";
 
 function ProductCard({product, handleAddToCart}){
 
